bfa153141b39bcaeb3454f23976b49b1f81ebe46
S-Arbor <seansutton7725@gmail.com>
2025-03-08 12:13:27 +1100

    Setting up architecture

---
+++ fried_liver_otb/board_vis.py
@@ -0,0 +1,22 @@
+
+
+
+def vision_thread(cam_url, move=True):
+
+    while True:
+        # get camera image
+        # display image
+        
+        # if my move:
+            # if time > time threshold:
+                # increment time threshold
+                # run motion detection
+
+        # motion check
+        # if the queue has an opponents move
+            # 
+        pass
+
+def motion_detect():
+    # save old
+    pass
